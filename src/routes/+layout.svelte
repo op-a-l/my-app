f4ec2bab34f79a93deb5be6de47751a21524d700
_op-a-l <149246377+op-a-l@users.noreply.github.com>
2024-11-21 12:49:32 +0100

    Keywords

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,10 +5,14 @@
 </script>
 
 <svelte:head>
-	<title>opal</title>
+	<title>_opal</title>
 	<meta
 		name="description"
 		content="opal est un compositeur de musique électronique basé à Rennes. Il explore l'expressivité des outils numériques et leur capacité à exprimer le réel. Bienvenue sur son site personnel."
+	/>
+	<meta
+		name="keywords"
+		content="musique, électronique, rennes, art, artiste, composition, sound design"
 	/>
 </svelte:head>
 
