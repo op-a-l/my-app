fef3500ac1ff9c596a38974e55d72f55a96d128e
_op-a-l <149246377+op-a-l@users.noreply.github.com>
2024-12-09 14:49:07 +0100

    accessibility increased a bit

--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -7,11 +7,13 @@
 	class="flex h-auto w-screen flex-row items-center justify-center p-5 md:justify-start"
 	id="contact"
 >
-	<button class="btn"><a href="mailto:opal.elektronaut@gmail.com"><Mail /></a></button>
-	<button class="btn"
+	<button class="btn" title="contact par courriel"
+		><a href="mailto:contact@opal-sounds.com"><Mail /></a></button
+	>
+	<button class="btn" title="page instagram"
 		><a href="https://www.instagram.com/_________opal/" target="_blank"><Instagram /></a></button
 	>
-	<button class="btn"
+	<button class="btn" title="chaîne youtube"
 		><a href="https://www.youtube.com/@opal.elektronaut/featured" target="_blank"><Youtube /></a
 		></button
 	>
