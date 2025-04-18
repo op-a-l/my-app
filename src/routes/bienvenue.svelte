4c130d5d39c7d4912e70c36291317658bc2ff2d7
mathis-archlinux <opal.elektronaut@gmail.com>
2025-04-18 15:38:51 +0200

    ajout bande demo

--- src/routes/bienvenue.svelte
+++ src/routes/bienvenue.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
+	import { Download } from 'lucide-svelte';
 </script>
 
 <main class="align-center flex h-screen flex-col items-center">
 	<section class="items-left flex h-screen w-auto flex-col justify-center p-10">
 		<h1 class="text-md md:text-2xl">Bonjour, je suis</h1>
-		<h1 class="font-mono text-3xl font-bold md:text-5xl">_opal,</h1>
-		<h1 class="text-md md:text-2xl">compositeur de musique électronique</h1>
+		<h1 class="font-humanist text-3xl font-bold md:text-5xl">_opal</h1>
+		<h1 class="text-md md:text-2xl">compositeur et sound designer</h1>
+	</section>
+	<section class="fixed bottom-20 md:bottom-10 z-50 flex-row gap-2 flex w-full justify-center">
+		<audio controls class="rounded-lg shadow-lg w-1/2">
+			<source src="/audio/bandeDemo.mp3" type="audio/mpeg" />
+			Votre navigateur ne supporte pas la lecture audio.
+		  </audio>
+		  <a
+		  href="/audio/bandeDemo.mp3"
+		  download="bandeDemo_opal.mp3"
+		  title="Télécharger la bande démo"
+	  >
+		  <Download size={15} />
+	  </a>
 	</section>
 </main>
--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -5,12 +5,12 @@
 	<nav
 		class="fixed top-0 mt-10 flex h-10 w-auto items-center justify-center rounded-xl border-2 border-dotted border-secondary-500 bg-transparent bg-opacity-50 p-5 px-10 shadow-xl backdrop-blur"
 	>
-		<ul class="flex gap-6 font-mono">
-			<li><a href="/" class="font-mono font-bold">0</a></li>
+		<ul class="flex gap-6 font-bold font-mono">
+			<li><a href="/">0</a></li>
 			<p>·</p>
-			<li><a href="#biographie" class="underline">1</a></li>
+			<li><a href="#biographie">1</a></li>
 			<p>·</p>
-			<li><a href="#musique" class="underline">2</a></li>
+			<li><a href="#musique">2</a></li>
 		</ul>
 	</nav>
 </main>
